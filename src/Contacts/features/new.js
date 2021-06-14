9519abbc14a00ba551327f8a8b35653b6988da60
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-14 15:05:43 +0000

    updated new.js

--- src/Contacts/features/new.js
+++ src/Contacts/features/new.js
@@ -1,32 +1,11 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View, StyleSheet } from "react-native";
 
 import { useSelector } from "react-redux";
 import { dataData } from "./features/dataSlice";
 
 import List from "./List";
 import ListInput from "./ListInput";
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
 
 function Contacts() {
   const dataList = useSelector(dataData);
