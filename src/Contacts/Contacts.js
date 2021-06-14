c389642809587acff05ae2ddd6d6a21cddfb5eba
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-14 13:54:45 +0000

    code update

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -4,30 +4,6 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-function ContactsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Contacts</Text>
-    </View>
-  );
-}
-
-function FavoritesScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Favorites!</Text>
-    </View>
-  );
-}
-
-function MeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Me!</Text>
-    </View>
-  );
-}
-
 const Tab = createBottomTabNavigator();
 
 function Contacts() {
