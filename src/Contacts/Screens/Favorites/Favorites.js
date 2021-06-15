51248b01da827145a3efc6c66878024891aea98e
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-06-15 16:07:16 +0000

    added Favorites

--- src/Contacts/Screens/Favorites/Favorites.js
+++ src/Contacts/Screens/Favorites/Favorites.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ContactList from "../../components/contactList";
+import ContactProfile from "../../components/contactProfile";
+
+const Stack = createStackNavigator();
 
 function FavoritesScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Favorites!</Text>
-    </View>
+    <Stack.Navigator initialRouteName="ContactList">
+      <Stack.Screen
+        name="Contacts"
+        component={ContactList}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="User"
+        component={ContactProfile}
+        options={({ route }) => ({ title: route.params.user })}
+      />
+    </Stack.Navigator>
   );
 }
+
 export default FavoritesScreen;
